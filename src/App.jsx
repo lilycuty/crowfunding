5585ed6b8d45b96cf7a4d3d1f3c023c0a28f6291
THAI <xinphatmutikill@gmail.com>
2023-09-21 12:25:08 +0700

    update react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import React, { Suspense } from 'react'
+import { Fragment } from 'react'
 import { useEffect } from 'react'
 import Modal from 'react-modal'
 import { useDispatch, useSelector } from 'react-redux'
-import { Routes, Route } from 'react-router-dom'
 import { authRefreshToken, authUpdateUser } from './store/auth/auth-slice'
 import { getToken, logOut } from './utils/auth'
-import RequiredAuthPage from './pages/RequiredAuthPage'
-import UnauthorizePage from './pages/UnauthorizePage'
-import { permissions } from './constants/permissions'
-const StartCampaignPage = React.lazy(() => import('./pages/StartCampaignPage'))
-const SignUpPage = React.lazy(() => import('./pages/SignUpPage'))
-const SignInPage = React.lazy(() => import('./pages/SignInPage'))
-const ShippingPage = React.lazy(() => import('./pages/ShippingPage'))
-const LayoutPayment = React.lazy(() => import('./layout/LayoutPayment'))
-const LayoutDashboard = React.lazy(() => import('./layout/LayoutDashboard'))
-const DashboardPage = React.lazy(() => import('./pages/DashboardPage'))
-const CheckoutPage = React.lazy(() => import('./pages/CheckoutPage'))
-const CampaignView = React.lazy(() => import('./modules/campaign/CampaignView'))
-const CampaignPage = React.lazy(() => import('./pages/CampaignPage'))
 
 Modal.setAppElement('#root')
 Modal.defaultStyles = {}
@@ -46,49 +32,51 @@ const App = () => {
     }
   }, [user, dispatch])
   return (
-    <Suspense>
-      <Routes>
-        <Route element={<LayoutDashboard></LayoutDashboard>}>
-          <Route path='/' element={<DashboardPage></DashboardPage>}></Route>
-          <Route
-            path='/unauthorize'
-            element={<UnauthorizePage></UnauthorizePage>}
-          ></Route>
-          <Route
-            path='/campaign'
-            element={<CampaignPage></CampaignPage>}
-          ></Route>
-          <Route
-            element={
-              <RequiredAuthPage
-                allowPermissions={[permissions.campaign.CREATE_CAMPAIGN]}
-              ></RequiredAuthPage>
-            }
-          >
+    <Fragment>
+      {/* <Suspense>
+        <Routes>
+          <Route element={<LayoutDashboard></LayoutDashboard>}>
+            <Route path='/' element={<DashboardPage></DashboardPage>}></Route>
             <Route
-              path='/start-campaign'
-              element={<StartCampaignPage></StartCampaignPage>}
+              path='/unauthorize'
+              element={<UnauthorizePage></UnauthorizePage>}
+            ></Route>
+            <Route
+              path='/campaign'
+              element={<CampaignPage></CampaignPage>}
+            ></Route>
+            <Route
+              element={
+                <RequiredAuthPage
+                  allowPermissions={[permissions.campaign.CREATE_CAMPAIGN]}
+                ></RequiredAuthPage>
+              }
+            >
+              <Route
+                path='/start-campaign'
+                element={<StartCampaignPage></StartCampaignPage>}
+              ></Route>
+            </Route>
+            <Route
+              path='/campaign/:slug'
+              element={<CampaignView></CampaignView>}
             ></Route>
           </Route>
-          <Route
-            path='/campaign/:slug'
-            element={<CampaignView></CampaignView>}
-          ></Route>
-        </Route>
-        <Route element={<LayoutPayment></LayoutPayment>}>
-          <Route
-            path='/checkout'
-            element={<CheckoutPage></CheckoutPage>}
-          ></Route>
-          <Route
-            path='/shipping-address'
-            element={<ShippingPage></ShippingPage>}
-          ></Route>
-        </Route>
-        <Route path='/register' element={<SignUpPage></SignUpPage>}></Route>
-        <Route path='/login' element={<SignInPage></SignInPage>}></Route>
-      </Routes>
-    </Suspense>
+          <Route element={<LayoutPayment></LayoutPayment>}>
+            <Route
+              path='/checkout'
+              element={<CheckoutPage></CheckoutPage>}
+            ></Route>
+            <Route
+              path='/shipping-address'
+              element={<ShippingPage></ShippingPage>}
+            ></Route>
+          </Route>
+          <Route path='/register' element={<SignUpPage></SignUpPage>}></Route>
+          <Route path='/login' element={<SignInPage></SignInPage>}></Route>
+        </Routes>
+      </Suspense> */}
+    </Fragment>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,102 @@
+/* eslint-disable react-refresh/only-export-components */
 import ReactDOM from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
 import { store } from './store/configureStore.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.scss'
+import { permissions } from './constants/permissions.js'
+import { Suspense } from 'react'
+const UnauthorizePage = React.lazy(() => import('./pages/UnauthorizePage'))
+const StartCampaignPage = React.lazy(() => import('./pages/StartCampaignPage'))
+const SignUpPage = React.lazy(() => import('./pages/SignUpPage'))
+const SignInPage = React.lazy(() => import('./pages/SignInPage'))
+const ShippingPage = React.lazy(() => import('./pages/ShippingPage'))
+const RequiredAuthPage = React.lazy(() => import('./pages/RequiredAuthPage'))
+const LayoutPayment = React.lazy(() => import('./layout/LayoutPayment'))
+const LayoutDashboard = React.lazy(() => import('./layout/LayoutDashboard'))
+const DashboardPage = React.lazy(() => import('./pages/DashboardPage'))
+const CheckoutPage = React.lazy(() => import('./pages/CheckoutPage'))
+const CampaignView = React.lazy(() => import('./modules/campaign/CampaignView'))
+const CampaignPage = React.lazy(() => import('./pages/CampaignPage'))
+
+const router = createBrowserRouter([
+  {
+    element: <LayoutDashboard />,
+    children: [
+      {
+        path: '/',
+        element: <DashboardPage />
+      },
+      {
+        path: '/campaign',
+        element: <CampaignPage />
+      },
+      {
+        path: '/unauthorize',
+        element: <UnauthorizePage />
+      },
+      {
+        element: (
+          <RequiredAuthPage
+            allowPermissions={[permissions.campaign.CREATE_CAMPAIGN]}
+          />
+        ),
+        children: [
+          {
+            path: '/start-campaign',
+            element: <StartCampaignPage />
+          }
+        ]
+      },
+      {
+        path: '/campaign/:slug',
+        element: <CampaignView />
+      }
+    ]
+  },
+  {
+    element: <LayoutPayment />,
+    children: [
+      {
+        path: '/checkout',
+        element: <CheckoutPage />
+      },
+      {
+        path: '/shipping-address',
+        element: <ShippingPage />
+      }
+    ]
+  },
+  {
+    path: '/register',
+    element: <SignUpPage></SignUpPage>
+  },
+  {
+    path: '/login',
+    element: <SignInPage></SignInPage>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-      <ToastContainer bodyClassName='font-primary text-sm'></ToastContainer>
-    </BrowserRouter>
+    <Suspense fallback={<p>Loading...</p>}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Suspense>
+    <ToastContainer bodyClassName='font-primary text-sm'></ToastContainer>
   </Provider>
 )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//       <ToastContainer bodyClassName='font-primary text-sm'></ToastContainer>
+//     </BrowserRouter>
+//   </Provider>
+// )
